12189be8118918a74cf2f808f98f75365529654b
James Laverack <james@jameslaverack.com>
2016-03-04 23:39:07 +0000

    Display dice in colour

--- edr.py
+++ edr.py
@@ -1,9 +1,32 @@
 #!/bin/python3
 import argparse
 import random
+import termcolor
 
 def d10():
     return random.randint(1, 10)
+
+def printn(p):
+    print(p, end = "")
+
+def display_dice(dice, target_number, hilight_ones = False):
+    dice.sort()
+    dice.reverse()
+    line = ""
+    for die in dice:
+        line += " "
+        if die == 10:
+            line += termcolor.colored(str(die), 'yellow', attrs = ['bold'])
+        elif die >= target_number:
+            line += termcolor.colored(str(die), attrs = ['bold'])
+        elif die == 1:
+            if hilight_ones:
+                line += termcolor.colored(str(die), 'red', attrs = ['bold'])
+            else:
+                line += str(die)
+        else:
+            line += str(die)
+    return line
 
 def count_successes(results, target_number):
     count = 0
@@ -13,18 +36,25 @@ def count_successes(results, target_number):
             count += 1
         if result == 10:
             double_count += 1
-    return [count, count + double_count]
+    return [count + double_count, count]
 
 def roll(dice, autos = 0, target_number = 7):
     # Roll the correct number of dice
     results = [d10() for x in range(0, dice)]
-    print(results)
-    normal, double = count_successes(results, target_number)
+    supernatural, mortal = count_successes(results, target_number)
 
-    normal += autos
-    double += autos
+    if supernatural == 0 and 1 in results:
+        # Botch
+        print(display_dice(results, target_number, hilight_ones = True))
+        print(termcolor.colored("Botch", 'red', attrs = ['bold']))
+    else:
+        # Apply auto successes
+        supernatural += autos
+        mortal += autos
 
-    print("Rolled {double} successes ({normal} without 10s giving two successes)".format(normal = normal, double = double))
+        print(display_dice(results, target_number))
+        print("Successes      : " + termcolor.colored(supernatural, 'yellow', attrs = ['bold']))
+        print("Mortals/Damage : " + termcolor.colored(mortal, attrs = ['bold']))
 
 def parse_dice_expression(epr):
     splr = epr.split("a")
