9c656c4f71eed2b9682aa49a69b51294b31b5b25
James Laverack <james@jameslaverack.com>
2016-03-04 19:05:05 +0000

    Implemented simple dice rolling

--- edr.py
+++ edr.py
@@ -1,5 +1,30 @@
 #!/bin/python3
 import argparse
+import random
+
+def d10():
+    return random.randint(1, 10)
+
+def count_successes(results, target_number):
+    count = 0
+    double_count = 0
+    for result in results:
+        if result >= target_number:
+            count += 1
+        if result == 10:
+            double_count += 1
+    return [count, count + double_count]
+
+def roll(dice, autos = 0, target_number = 7):
+    # Roll the correct number of dice
+    results = [d10() for x in range(0, dice)]
+    print(results)
+    normal, double = count_successes(results, target_number)
+
+    normal += autos
+    double += autos
+
+    print("Rolled {double} successes ({normal} without 10s giving two successes)".format(normal = normal, double = double))
 
 def parse_dice_expression(epr):
     splr = epr.split("a")
@@ -17,5 +42,5 @@ if __name__ == "__main__":
     dice, autos = parse_dice_expression(args.dice)
 
     print("Rolling {dice} dice with {autos} automatic successes".format(dice = dice, autos = autos))
-
+    roll(dice, autos = autos)
     
